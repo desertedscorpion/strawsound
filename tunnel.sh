b45c3630d8b4c039b4d1e43accb8d2830868208b
Emory Merryman <emory.merryman+2N61ZmeAFB9TKDIP@gmail.com>
2016-03-24 10:46:55 +0000

    fixed bug with tunnel command

--- tunnel.sh
+++ tunnel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source /usr/local/src/private/credentials.sh &&
-    ssh -i docker.pem -L 127.0.0.1:8080:127.0.0.1:8080 -o UseStrictHostKeyChecking=no -N -l fedora $(vagrant ssh initial -- curl http://instance-data/latest/meta-data/public-ipv4) &&
+    ssh -i docker.pem -L 127.0.0.1:8080:127.0.0.1:8080 -o StrictHostKeyChecking=no -N -l fedora $(vagrant ssh initial -- curl http://instance-data/latest/meta-data/public-ipv4) &&
     true
