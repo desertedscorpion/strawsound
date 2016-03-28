98fb627470acfa615c8ef388a98735cb0b591ac1
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-03-28 13:15:05 -0400

    problem with branch tag

--- provision.sh
+++ provision.sh
@@ -85,8 +85,8 @@ Then we will create a new one.
 That way we can be sure there is only exactly one.
 EOF
     ) &&
-    INSTANCE_ID=$(/usr/local/bin/aws ec2 describe-instances --filters "Name=tag:Name,Values=docker" "Name=tag:Environment,Values=${ENVIRONMENT}" "Name=tag:Environment,Values=${BRANCH}" "Name=instance-state-name,Values=running" | grep INSTANCES | cut --fields 8) &&
-    /usr/local/bin/aws ec2 describe-volumes --filters "Name=tag:Name,Values=docker" "Name=tag:Environment,Values=${ENVIRONMENT}" "Name=tag:Environment,Values=${BRANCH}"  | grep VOLUMES | wc --lines | grep VOLUMES | cut --fields 2 | while read VOLUME_ID
+    INSTANCE_ID=$(/usr/local/bin/aws ec2 describe-instances --filters "Name=tag:Name,Values=docker" "Name=tag:Environment,Values=${ENVIRONMENT}" "Name=tag:Branch,Values=${BRANCH}" "Name=instance-state-name,Values=running" | grep INSTANCES | cut --fields 8) &&
+    /usr/local/bin/aws ec2 describe-volumes --filters "Name=tag:Name,Values=docker" "Name=tag:Environment,Values=${ENVIRONMENT}" "Name=tag:Branch,Values=${BRANCH}"  | grep VOLUMES | wc --lines | grep VOLUMES | cut --fields 2 | while read VOLUME_ID
     do
 	echo /usr/local/bin/aws ec2 delete-volume --volume-id ${VOLUME_ID} &&
 	    /usr/local/bin/aws ec2 delete-volume --volume-id ${VOLUME_ID} &&
