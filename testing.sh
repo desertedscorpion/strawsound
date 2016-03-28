ce238da2aa3c2a173b12ccb1594ddeb908d9a962
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-03-28 11:59:25 -0400

    fixed ampersand

--- testing.sh
+++ testing.sh
@@ -86,7 +86,7 @@ EOF
     [[ ${GITNAME} == $(vagrant ssh testing -- grep name .gitconfig | sed -e "s#^\s*name\s*=\s*##") ]] &&    
     [[ ${GITEMAIL} == $(vagrant ssh testing -- grep email .gitconfig | sed -e "s#^\s*email\s*=\s*##") ]] &&
     echo let us dockerize for verification &&
-    vagrant ssh testing -- mkdir --parents /home/fedora/testing/desertedscorpion
+    vagrant ssh testing -- mkdir --parents /home/fedora/testing/desertedscorpion &&
 echo Let us test with a simple node express hello world application &&
     vagrant ssh testing -- git -C /home/fedora/testing/desertedscorpion clone git@github.com:desertedscorpion/subtleostrich.git &&
     echo build the docker image from the Dockerfile &&
