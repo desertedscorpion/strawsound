178923a55f9ce0e6bc0345739de128d920f1f5bd
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-04-01 16:18:08 -0400

    fixed using

--- using.sh
+++ using.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-source /usr/local/src/private/credentials.sh &&
+export ACCESS_KEY_ID=$(cat private/initial/aws/access_key_id) &&
+    export SECRET_ACCESS_KEY=$(cat private/initial/aws/secret_access_key) &&
     vagrant ssh initial &&
     true
