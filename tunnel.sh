753fd616af63c64ee69ee19985f69e0b1473a4c4
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-04-01 21:11:05 -0400

    do not use UseStrictHostKeyChecking

--- tunnel.sh
+++ tunnel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-    ssh -i private/initial/aws/docker.pem -L 127.0.0.1:8080:127.0.0.1:8080 -o UseStrictHostKeyChecking=no -N -l fedora $(vagrant ssh initial -- curl http://instance-data/latest/meta-data/public-ipv4) &&
+    ssh -i private/initial/aws/docker.pem -L 127.0.0.1:8080:127.0.0.1:8080 -o StrictHostKeyChecking=no -N -l fedora $(vagrant ssh initial -- curl http://instance-data/latest/meta-data/public-ipv4) &&
     true
