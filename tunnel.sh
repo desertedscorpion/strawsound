fab04f7ffaa489d764e577d8a346ffda9b3459c5
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-04-01 21:13:22 -0400

    fixed tunnel with export

--- tunnel.sh
+++ tunnel.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+export BRANCH=$(git rev-parse --abbrev-ref HEAD) &&
+    export DOCKER_USERID=$(cat private/initial/docker/docker_userid) &&
+    export DOCKER_PASSWORD=$(cat private/initial/docker/docker_password) &&
+    export DOCKER_EMAIL=$(cat private/initial/docker/docker_email) &&
+    export ACCESS_KEY_ID=$(cat private/initial/aws/access_key_id) &&
+    export SECRET_ACCESS_KEY=$(cat private/initial/aws/secret_access_key) &&
+    export GITHUB_STRAWSOUND_PRIVATE_SSH_KEY=$(cat private/initial/github/strawsound_id_rsa) &&
+    export GITHUB_STRAWSOUND_PUBLIC_SSH_KEY=$(cat private/initial/github/strawsound_id_rsa.pub) &&
+    export GITNAME=$(cat private/initial/git/name) &&
+    export GITEMAIL=$(private/initial/git/email.sh) &&
     ssh -i private/initial/aws/docker.pem -L 127.0.0.1:8080:127.0.0.1:8080 -o StrictHostKeyChecking=no -N -l fedora $(vagrant ssh initial -- curl http://instance-data/latest/meta-data/public-ipv4) &&
     true
