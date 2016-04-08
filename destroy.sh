d6ff80684f12ff9da50b2b85a09a15d0c4b35381
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-04-08 17:32:45 -0400

    fixed destroy script

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-source /usr/local/src/private/credentials.sh &&
+export BRANCH=$(git rev-parse --abbrev-ref HEAD) &&
+    export DOCKER_USERID=$(cat private/initial/docker/docker_userid) &&
+    export DOCKER_PASSWORD=$(cat private/initial/docker/docker_password) &&
+    export DOCKER_EMAIL=$(cat private/initial/docker/docker_email) &&
+    export ACCESS_KEY_ID=$(cat private/initial/aws/access_key_id) &&
+    export SECRET_ACCESS_KEY=$(cat private/initial/aws/secret_access_key) &&
+    export GITHUB_STRAWSOUND_PRIVATE_SSH_KEY=$(cat private/initial/github/strawsound_id_rsa) &&
+    export GITHUB_STRAWSOUND_PUBLIC_SSH_KEY=$(cat private/initial/github/strawsound_id_rsa.pub) &&
+    export GITNAME=$(cat private/initial/git/name) &&
+    export GITEMAIL=$(private/initial/git/email.sh) &&
     (
 	vagrant destroy --force initial ||
 	    echo "I really do not know why this fails from time to time, but as long as the instance is destroyed it is OK"
